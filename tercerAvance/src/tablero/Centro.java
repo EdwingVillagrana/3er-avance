6c4bc6c8bd4e6de2b84436eed4491fae6bb33b57
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-10-05 17:22:12 -0700

    Comentarios
    Se agregaron comentarios en algunas clases

--- tercerAvance/src/tablero/Centro.java
+++ tercerAvance/src/tablero/Centro.java
@@ -2,26 +2,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package tablero;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  *
  * @author edw_v
  */
-public class Centro implements Graphic{
-    
+public class Centro implements Graphic {
+
     private List<Graphic> casillasCentrales;
-    
-    public void paint(Graphics g){
+
+    /**
+     * Método obligatorio que utiliza el panel para dibujar un componente.
+     *
+     * @param g instancia de tipo Graphics para poder dibujar en el JPanel.
+     */
+    public void paint(Graphics g) {
         dibujar(g);
     }
 
+    /**
+     * Método que inicializa la lista a un arraylist y lo llena con las casillas
+     * centrales, las cuales siempre tendrán las mismas coordenadas
+     *
+     * @param g instancia de tipo Graphics para poder dibujar en el JPanel.
+     */
     @Override
     public void dibujar(Graphics g) {
         casillasCentrales = new ArrayList<Graphic>();
@@ -29,7 +38,7 @@ public class Centro implements Graphic{
         casillasCentrales.add(new CasillaCentral(430, 410));
         casillasCentrales.add(new CasillaCentral(480, 460));
         casillasCentrales.add(new CasillaCentral(480, 410));
-        
+
         for (Graphic casilla : casillasCentrales) {
             casilla.dibujar(g);
         }
